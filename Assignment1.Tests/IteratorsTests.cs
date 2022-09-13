8c4274e83d6ae0b6db17d741a45dc2540177d685
frmo24042 <93861344+frmo24042@users.noreply.github.com>
2022-09-13 12:11:19 +0200

    Added solutions to first half of assignment 1 (#1)

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,5 +1,34 @@
+using Xunit;
+using Assignment1;
+
 namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+    [Fact]
+    public void Given_a_stream_returns_flattenStream()
+    {
+        // Given
+        var stream = new[] { new[] { 'T', 'T', 'T'}, new[] { 'T', 'T', 'T'} };
+    
+        // When
+        var flatten = Assignment1.Iterators.Flatten(stream);
+    
+        // Then
+        Assert.Equal(new[] {'T', 'T', 'T', 'T', 'T', 'T'}, flatten);
+    }
+
+    [Fact]
+    public void Given_an_intStream_returns_evens()
+    {
+        // Given
+        var stream = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        var predicate = new Predicate<int>(i => i % 2 == 0);
+
+        // When
+        var filter = Assignment1.Iterators.Filter(stream, predicate);
+    
+        // Then
+        Assert.Equal(new[] {2, 4, 6, 8, 10}, filter);
+    }
 }
